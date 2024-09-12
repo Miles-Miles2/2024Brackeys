e4d249508959b048b1789111decbe89e714a66de
Spunboing <diamondjellybean457@gmail.com>
2024-09-11 22:50:03 -0700

    Mummy now moves toward player

--- Scipts/enemy.gd
+++ Scipts/enemy.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
-@onready var ray_cast_right = $RayCastRight
-@onready var ray_cast_left = $RayCastLeft
-@onready var slime = $AnimatedSprite2D
-@onready var body = $"."
+@onready var ray_cast_right = $enemyBody2D/RayCastRight
+@onready var ray_cast_left = $enemyBody2D/RayCastLeft
+@onready var slime = $enemyBody2D/AnimatedSprite2D
+@onready var body = $enemyBody2D
 @onready var footsteps = $footsteps
+@onready var player = $"../Player"
+@onready var slim = $"."
+
+
+
+@export var crystal_collected = true
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 #signal animation_done
@@ -14,34 +20,51 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 
-var direction = 1
+var direction = -1
 var delay = 0
 	
 func _physics_process(delta):
+	
+
+	var mummy_x = body.global_position.x
+	var player_x = player.global_position.x
+	
+	print (player_x)
+	print(mummy_x)
+
 	if delay < 0.5:
 		delay += delta
 	else:
 		if not body.is_on_floor():
 			body.velocity.y += gravity * delta
 		
-		if ray_cast_right.is_colliding():
-			direction = -1
-			slime.flip_h = not(slime.flip_h)
+		if not(crystal_collected):
+			if ray_cast_right.is_colliding():
+				direction = -1
+				slime.flip_h = not(slime.flip_h)
+				
+				
+			if ray_cast_left.is_colliding():
+				direction = 1
+				slime.flip_h = not(slime.flip_h)
+				
+		
+		else:
+			if (player_x < mummy_x):
+				direction = -1
+				slime.flip_h = false
+				
+				
+			elif (player_x > mummy_x):
+				direction = 1
+				slime.flip_h = true
 			
-			
-		if ray_cast_left.is_colliding():
-			direction = 1
-			slime.flip_h = not(slime.flip_h)
-			
-
 		body.velocity.x = (SPEED * delta * direction)
+			
 
 		
 
 		body.move_and_slide()
 
 
-func _on_ground_check_body_exited(collidedBody: Node2D) -> void:
-	if (collidedBody.is_in_group("ground")):
-		slime.flip_h = not(slime.flip_h)
-		direction *= -1
+
